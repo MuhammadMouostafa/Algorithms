87afae31915f10b84ad17e9f80cc0e097ad717df
Muhammad_Mostafa <mohamedmoustafaa1998@gmail.com>
2023-12-31 22:11:21 +0200

    init db

---
+++ dinamic_programming/UVA - 10192.cpp
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifdef __GNUC__
+# define ffs(x) __builtin_ffs(x)
+#elif __INTEL_COMPILER
+# define ffs(x) _bit_scan_forward(x)
+#endif
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename key>
+using ordered_set = tree<key, null_type, less<key>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+using ll = long long;
+#define all(v) v.begin(),v.end()
+#define EPS 1e-10
+#define INF 1e18
+#define PI acos(-1.0)
+#define sz(s)    (int)(s.size())
+#define endl "\n"
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+#endif
+}
+void fast()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+template<class val>
+istream& operator>>(istream& is, vector<val>& vec) {
+    for (int i = 0; i < vec.size(); i++)
+        is >> vec[i];
+    return is;
+}
+template<class val>
+ostream& operator<<(ostream& os, vector<val>& vec) {
+    for (int i = 0; i < sz(vec); i++) {
+        os << vec[i];
+        if (i + 1 < sz(vec))
+            os << ' ';
+    }
+    return os;
+}
+template<class f, class s>
+istream& operator>>(istream& is, pair<f, s>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+template<class f, class s>
+ostream& operator<<(ostream& os, pair<f, s>& p) {
+    os << "{" << p.first << ',' << p.second << "}";
+    return os;
+}
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+int dx[] = {-1, 0, 1, 0, 1, 1, -1, -1};
+int dy[] = {0, 1, 0, -1, 1, -1, 1, -1};
+char dir[] = {'U', 'R', 'D', 'L'};
+
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+const int N = 1e5 + 10;
+const int MOD = 1e9 + 7;
+
+int n,m;
+string s,a;
+vector<vector<int>> mem;
+int solve(int i, int j){
+   if(i==n||j==m)return 0;
+
+    int &ref=mem[i][j];
+    if(~ref)return ref;
+
+    return ref=max({solve(i+1,j),solve(i,j+1),(s[i]==a[j])+solve(i+1,j+1)});
+}
+void Main(const int &T){
+    int t=0;
+    while(getline(cin,s)){
+        if(s[0]=='#')return;
+        t++;
+        getline(cin,a);
+        n=s.size();
+        m=a.size();
+        mem=vector<vector<int>>(n,vector<int>(m,-1));
+        cout<<"Case #"<< t <<": you can visit at most ";
+        cout<<solve(0,0)<<" cities."<<endl;
+    }
+}   
+int main()
+{
+    file();
+    fast();
+    int T = 1;
+    // cin>>T;
+    for(int t=1; t<=T; t++){
+        // cout<<"Case "<< t <<": ";
+        Main(t);
+    }
+}
