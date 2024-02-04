4d9a34282a2d5999944742dda4bea7b8e0cf94ae
Muhammad_Mostafa <mohamedmoustafaa1998@gmail.com>
2024-02-04 23:17:51 +0200

    add dsu aproblem

---
+++ graph/CodeForces - 356A.cpp
@@ -0,0 +1,178 @@
+    #define _CRT_SECURE_NO_WARNINGS
+#ifdef __GNUC__
+# define ffs(x) __builtin_ffs(x)
+#elif __INTEL_COMPILER
+# define ffs(x) _bit_scan_forward(x)
+#endif
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename key>
+using ordered_set = tree<key, null_type, less<key>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+using ll = long long;
+#define all(v) v.begin(),v.end()
+#define EPS 1e-10
+#define INF 1e18
+#define PI acos(-1.0)
+#define sz(s)    (int)(s.size())
+#define endl "\n"
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    // freopen("output.out", "w", stdout);
+#endif
+}
+void fast()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+template<class val>
+istream& operator>>(istream& is, vector<val>& vec) {
+    for (int i = 0; i < vec.size(); i++)
+        is >> vec[i];
+    return is;
+}
+template<class val>
+ostream& operator<<(ostream& os, vector<val>& vec) {
+    for (int i = 0; i < sz(vec); i++) {
+        os << vec[i];
+        if (i + 1 < sz(vec))
+            os << ' ';
+    }
+    return os;
+}
+template<class f, class s>
+istream& operator>>(istream& is, pair<f, s>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+template<class f, class s>
+ostream& operator<<(ostream& os, pair<f, s>& p) {
+    os << "{" << p.first << ',' << p.second << "}";
+    return os;
+}
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+// int dx[] = {-1, 0, 1, 0, 1, 1, -1, -1};
+// int dy[] = {0, 1, 0, -1, 1, -1, 1, -1};
+int dx[] = {-1,-1,0, 1, 1,1, 0,-1};
+int dy[] = {0, 1, 1, 1, 0,-1, -1,-1};
+
+char dir[] = {'U', 'R', 'D', 'L'};
+
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+const int N = 2e5 + 10;
+const int MOD = 1e9 + 7;
+
+struct DSU {
+	vector<int> rank, parent, size,mx;
+	// vector<vector<int>> component;
+	int forsets;
+	DSU(int n) {
+		size = rank = parent = vector<int>(n + 1, 1);
+		mx = vector<int>(n + 1, 0);
+		// component = vector<vector<int>>(n + 1);
+		forsets = n;
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+			mx[i]=i;
+			// component[i].push_back(i);
+		}
+	}
+	int find_set(int v) {
+		if (v == parent[v])
+			return v;
+		return parent[v] = find_set(parent[v]);
+	}
+	void link(int par, int node, int mxx) {
+		if(mxx==-1){
+			mxx=max(mx[node],mx[par]);
+		}
+		mx[node]=mx[par]=mxx;
+		parent[node] = par;
+		size[par] += size[node];
+		// for (const int& it : component[node])
+		// 	component[par].push_back(it);
+		// component[node].clear();
+		if (rank[par] == rank[node])
+			rank[par]++;
+		forsets--;
+	}
+	bool union_sets(int v, int u, int mxx=-1) {
+		v = find_set(v), u = find_set(u);
+		if (v != u) {
+			if (rank[v] < rank[u])
+				swap(v, u);
+			link(v, u,mxx);   
+		}
+		return v != u;
+	}
+	bool same_set(int v, int u) {
+		return find_set(v) == find_set(u);
+	}
+	int size_set(int v) {
+		return size[find_set(v)];
+	}
+};
+
+
+void Main(const int &T){
+	int n,m;
+	cin>>n>>m;	
+	DSU rieght(n);
+	DSU winer(n);
+	vector<int> ans(n+1);
+	
+	while(m--){
+		int L,R,W;
+		cin>>L>>R>>W;
+		int l=L,r=R;
+		while(l<=R){
+			r=rieght.mx[rieght.find_set(l)];
+			
+			int w=winer.mx[winer.find_set(r)];
+			
+			if(w>=L&&w<=R&&w!=W){
+				ans[w]=W;
+				winer.union_sets(w,W,W);
+			}
+
+			if(r+1<=R){
+				rieght.union_sets(r,r+1);
+			}
+			
+			l=r+1;
+		}
+	}
+
+		
+	for(int i=1; i<=n; i++){
+		cout<<ans[i]<<" ";
+	}
+	cout<<endl;
+}
+int main()
+{
+    file();
+    fast();
+    int T = 1;
+    // cin>>T;
+    for(int t=1; t<=T; t++){
+        // cout<<"Case "<< t <<":";
+        Main(t);
+    }
+}
